0387a93e5c2d92d398ffe506a14c5f67be3c11c4
Anstice <s410378047@gm.ntpu.edu.tw>
2018-01-20 11:39:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/a20180120_01/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.student.a20180120_01;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn2 = findViewById(R.id.button2);
+        MyListener m = new MyListener();
+        btn2.setOnClickListener(m);
+    }
+
+    class MyListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this, "Click Click", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    public void click1 (View v) {
+        TextView tv = (TextView) findViewById (R.id.textView);
+        tv.setText("Hello World!");
+        tv.setTextColor(0xff000000);
+        //tv.setTextColor(Color.RED);
+    }
+
+}
